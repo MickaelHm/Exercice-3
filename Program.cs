62632e724f5b8c0512614a4b5a86bb7d84adad69
MickaelHm <harmeau.mickael@gmail.com>
2018-12-17 14:46:53 +0100

    Exercice complet

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("Ecrivez un nombre puis appuyer sur entrée");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ecrivez un nombre puis appuyer syr entrée");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("a=" + a + " et b=" + b);
+
+        }
+    }
+}
